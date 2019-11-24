145cb9eb2ad09ffc8809e40f415744c663ee001a
Javier Sánchez <info@entropia.studio>
2019-11-24 08:50:54 +0100

    Add .env file to project

--- src/app/config/database.js
+++ src/app/config/database.js
@@ -1,6 +1,5 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mondoDB = require('./access');
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
