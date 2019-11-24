ba34c7830dc349a95c6b306b225a1eee022e3f52
Javier Sánchez <info@entropia.studio>
2019-11-24 08:02:32 +0100

    filterStudents correction, take into account teacher selected. Add loader spinner to shared. Fix routing for default domain.

--- src/app/dashboard/shared/sharedmodule.module.ts
+++ src/app/dashboard/shared/sharedmodule.module.ts
@@ -1,30 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StudentComponent } from './components/student/student.component';
-import { InputSearchComponent } from './components/input-search/input-search.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 // material
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import {MatInputModule} from '@angular/material/input';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
+// custom components
+import { SpinnerComponent } from './components/spinner/spinner.component'; 
+import { StudentComponent } from './components/student/student.component';
+import { InputSearchComponent } from './components/input-search/input-search.component';
 
 @NgModule({
   declarations: [
     StudentComponent,
-    InputSearchComponent
+    InputSearchComponent,
+    SpinnerComponent
   ],
   imports: [
     CommonModule,
     MatDividerModule,
     MatExpansionModule,    
     ReactiveFormsModule,
-    MatInputModule
+    MatInputModule,
+    MatProgressSpinnerModule
   ],
   exports: [
     StudentComponent,
-    InputSearchComponent
+    InputSearchComponent,
+    SpinnerComponent
   ]
 })
 export class SharedmoduleModule { }
