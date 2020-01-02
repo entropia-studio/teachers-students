48a464c02ded56334bda6dc803c6c1936f247543
Javier Sánchez <info@entropia.studio>
2020-01-02 12:00:08 +0100

    Add logic to logout user and redirect in case the JWT have been expired

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,19 +1,22 @@
 import { AuthService } from './auth/login/services/auth.service';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
     constructor(
-        private authService: AuthService
+        private authService: AuthService,
+        private router: Router
     ){}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
       
     if (req.url.includes('teachers') || req.url.includes('students')){
-        
+
         const currentUser = this.authService.currentUserValue;
         
         if (currentUser){
@@ -22,7 +25,17 @@ export class AuthInterceptor implements HttpInterceptor {
             headers: req.headers.set('x-access-token', currentUser.token)
           });
 
-          return next.handle(authReq);
+          return next.handle(authReq).pipe(
+            tap((resp: any) => {                                          
+              if (resp.body){
+                const {status} = resp.body;  
+                if (status === 'error'){
+                  this.authService.logout();                  
+                  this.router.navigate(['/auth/login']);
+                }
+              }
+            })
+          );
         }
     }
 
